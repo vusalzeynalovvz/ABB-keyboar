029b6d593f790945160d23487ebcbba0dd7f806b
vusalzeynalovvz <112488906+vusalzeynalovvz@users.noreply.github.com>
2022-12-10 20:13:40 +0400

    Add files via upload

---
+++ ABB,KEYBOARD/app.js
@@ -0,0 +1,40 @@
+// // document.getElementById("search").onclick = function() {  
+// //     fun()  
+// // }
+// // function fun() {  
+// //     document.getElementById("yardim").style.display = "block"   
+// // }
+
+
+// Keyboard
+
+
+// let circle = document.querySelector('.circle');
+// let move = 10;
+
+// window.addEventListener('load', () => {
+//     circle.style.position = 'absolute';
+//     circle.style.left = 0;
+//     circle.style.top = 0;
+// });
+
+
+// window.addEventListener('keydown', (e) => {
+//     switch (e.keyCode) {
+//         case 37:
+//             circle.style.left = parseInt(circle.style.left) - move + 'px';
+//             break;
+//         case 39:
+//             circle.style.left = parseInt(circle.style.left) + move + 'px';
+//             break;
+//         case 38:
+//             circle.style.top = parseInt(circle.style.top) - move + 'px';
+//             break;
+//         case 40:
+//             circle.style.top = parseInt(circle.style.top) + move + 'px';
+//             break;
+//     }
+// });
+
+
+
